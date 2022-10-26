2cad63913bb37980bf42699d75db866a687c681a
Hristian Balevski <114162692+HristianBalevski@users.noreply.github.com>
2022-10-26 09:42:09 +0100

    Update Find_The_Runner_Up_Score.py

--- Python/Find_The_Runner_Up_Score.py
+++ Python/Find_The_Runner_Up_Score.py
@@ -19,7 +19,7 @@
 # 5
 
 # Explanation 0
-# Given list is [2, 3, 6, 6, 5] The maximum score is , second maximum is . Hence, we print  as the runner-up score.
+# Given list is [2, 3, 6, 6, 5] The maximum score is , second maximum is 5. Hence, we print 5 as the runner-up score.
 
 n = int(input())
 arr = map(int, input().split())
