b2fa870f5b24cdc5684a5b0e551478ba28bc27f4
Hristian Balevski <114162692+HristianBalevski@users.noreply.github.com>
2022-10-26 11:52:07 +0100

    Update Lists.py

--- Python/Lists.py
+++ Python/Lists.py
@@ -1,5 +1,5 @@
 # Consider a list (list = []). You can perform the following commands:
-# 1. insert i e: Insert integer  at position .
+# 1. insert i e: Insert integer  at position.
 # 2. print: Print the list.
 # 3. remove e: Delete the first occurrence of integer e.
 # 4. append e: Insert integer e at the end of the list.
@@ -11,7 +11,7 @@
 # Iterate through each command in order and perform the corresponding operation on your list.
 
 # Input Format
-# The first line contains an integer, , denoting the number of commands. 
+# The first line contains an integer,n, denoting the number of commands. 
 # Each line  of the  subsequent lines contains one of the commands described above.
 
 # Constraints
